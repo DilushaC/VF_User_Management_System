2ed7860db74cafc4990416142ec10cb740d982b5
4537 <4537@VF-CO-IT-DEV-11>
2025-12-09 14:46:21 +0530

    Implement concrete class for menu item iteration to prevent exception of anonymous attributes

---
+++ UserManagement.Data/Models/MenuItemModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserManagement.Data.Models
+{
+    public class MenuItemModel
+    {
+        public string MenuTitle { get; set; } = string.Empty;
+        public string PageTitle { get; set; } = string.Empty;
+        public string Url { get; set; } = string.Empty;
+        public string Permission { get; set; } = "false"; 
+    }
+
+}
