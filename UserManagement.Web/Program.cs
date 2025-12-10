934c093781b1d95b16c6163ab1006e65eaa219ae
4537 <4537@VF-CO-IT-DEV-11>
2025-12-10 11:00:05 +0530

    Icon test for sub menu and page padding fixed

--- UserManagement.Web/Program.cs
+++ UserManagement.Web/Program.cs
@@ -43,7 +43,7 @@ builder.Services.AddScoped<ADAuthentication>();
 builder.Services.AddDistributedMemoryCache();
 builder.Services.AddSession(options =>
 {
-    options.IdleTimeout = TimeSpan.FromMinutes(60);
+    options.IdleTimeout = TimeSpan.FromMinutes(10);
     options.Cookie.HttpOnly = true;
     options.Cookie.IsEssential = true;
     options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
